8fda678b066903a9ed1aec6b91a280ca2bb4bcd8
MobikTech <62842262+MobikTech@users.noreply.github.com>
2021-12-02 15:55:16 +0200

    deleting

--- FinanceManager.PL.MVC/Models/AccountViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FinanceManager.PL.MVC.Models
-{
-    public sealed class AccountViewModel
-    {
-        public string Number { get; set; }
-        
-        public decimal Count { get; set; }
-    }
-}
--- FinanceManager.PL.MVC/Models/CategoryViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace FinanceManager.PL.MVC.Models
-{
-    public sealed class CategoryViewModel
-    {
-        public string Name { get; set; }
-    }
-}
